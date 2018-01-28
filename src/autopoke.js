25c233a127b49223abcb7687559703d8c55a62b6
M. Romain <maximilienromain96@hotmail.com>
2018-01-28 19:06:44 +0100

    adding time print

--- src/autopoke.js
+++ src/autopoke.js
@@ -24,6 +24,14 @@ function poke()
             nbPokesDiv.innerHTML = parseInt(nbPokesDiv.innerHTML) +1;
             elt_link.click();
         }
+        var currentdate = new Date(); 
+        var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+        console.log(datetime)
     }
     
     /* Make it more real */
